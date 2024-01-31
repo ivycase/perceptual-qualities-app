86b0f9de899eeb33e47f2c33c018da756534ae76
Ivy Case <ivycase@berkeley.edu>
2024-01-30 19:16:06 -0800

    Rebuild Amplify for new (experimental) AWS setup

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,8 +1,10 @@
 export type AmplifyDependentResourcesAttributes = {
   "auth": {
-    "voiceappauth": {
+    "perceptualqualities3e683ef6": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
